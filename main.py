7e54377c83b6517548125a018fdb0c95c1024dea
7ziegen <7ziegen@gmail.com>
2020-06-23 23:41:11 +0200

    Fix the code.

--- main.py
+++ main.py
@@ -7,14 +7,15 @@ from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
 from scipy.ndimage import gaussian_filter
 
-filename_sub_left = 'sim_flying_room_stereo/cam0/events.txt'
-filename_sub_right = 'sim_flying_room_stereo/cam1/events.txt'
+filename_sub_left = 'C:/Users/7zieg/Downloads/BioVision/cam0/events.txt'
+filename_sub_right = 'C:/Users/7zieg/Downloads/BioVision/cam1/events.txt'
 
-max_x = 180
-max_y =  240
-buffer_right = np.empty((max_x, 0)).tolist()
+max_y = 180
+max_x = 240
+dis = 10
+buffer_right = np.empty((max_y, 0)).tolist()
 maximumTimeDifference = 0.0001
-wmi = np.zeros((max_y,max_x,240))
+wmi = np.zeros((max_y, max_x, dis))
 search_min = 0
 
 def addtoBufferRight(data):
@@ -29,16 +30,17 @@ def searchEvent(data):
     pol = int(data[3])
     foundEvents = []
     global search_min
-    for value in range(search_min,len(buffer_right[y_value])):
+    for x in range(search_min,len(buffer_right[y_value])):
     #for value in enumerate(buffer_right[y_value]):
-        if timestamp < buffer_right[y_value][value][0]:
+        if timestamp < buffer_right[y_value][x][0]:
             break
-        elif timestamp - buffer_right[y_value][value][0] > maximumTimeDifference:
-            search_min = value
-        elif pol == buffer_right[y_value][value][3] and timestamp - buffer_right[y_value][value][0] < maximumTimeDifference:
-            foundEvents.append(buffer_right[y_value][value])
-        else:
+        elif timestamp - buffer_right[y_value][x][0] > maximumTimeDifference:
             continue
+        elif abs(data[1] - buffer_right[y_value][x][1]) > (dis - 1):
+            continue
+        elif pol == buffer_right[y_value][x][3]:
+            foundEvents.append(buffer_right[y_value][x])
+
     return foundEvents
 
 def calculateMatchingCosts(timestampleft, timestampright, weightingfunction = 1):
@@ -57,9 +59,9 @@ def calculateMatchingCosts(timestampleft, timestampright, weightingfunction = 1)
     else:
         return timediff
 
-image = np.loadtxt(fname="sim_flying_room_stereo\cam0\groundtruth.txt", delimiter=' ', dtype=np.float)[:]
+image = np.loadtxt(fname="C:/Users/7zieg/Downloads/BioVision/cam0/groundtruth.txt", delimiter=' ', dtype=np.float)[:]
 
-with open("sim_flying_room_stereo\cam0\groundtruth.txt") as file:
+with open("C:/Users/7zieg/Downloads/BioVision/cam0/groundtruth.txt") as file:
     array2d = [[float(digit) for digit in line.split()] for line in file]
 
 #print(array2d.shape)
@@ -73,12 +75,12 @@ events_right =np.genfromtxt(fname=filename_sub_right, delimiter=' ', dtype=np.fl
 
 
 #write Events into Buffer
-for i in range(events_right.shape[0]):
+n = events_right.shape[0]
+for i in range(n):
     addtoBufferRight(events_right[i,:])
-
-for i in range(events_left.shape[0]):
+n = events_left.shape[0]
+for i in range(n):
 #for i in range(200):
-    foundevents = []
     foundevents = searchEvent(events_left[i,:])
     #print(events_left[i,:])
     #print(foundevents)
@@ -87,24 +89,22 @@ for i in range(events_left.shape[0]):
     #wmi = wmi - 0.1
     #wmia = wmi.clip(min=0)
 
-
-    if not len(foundevents) == 0:
-        for j in range(len(foundevents)):
-            costs = calculateMatchingCosts(events_left[i,0],foundevents[j][0])*100000
-            if costs < 0:
-                print("start")
-                print(events_left[i,:])
-                print(foundevents[j][:])
-                print("end")
-            disparity = int(abs(events_left[i][1]-foundevents[j][1]))
-            wmi[int(events_left[i][1]),int(events_left[i][2]),disparity] = costs
-    #if i%500 == 0:
-    #    wmi = wmi *0.7
-    #     wmia = wmi.clip(min=0)
-          #tmp = gaussian_filter(wmi, sigma=1)
-          #disp = np.argmax(tmp, 2)
-          #plt.imshow(disp, cmap = "hot")
-          #plt.show()
+    for j in range(len(foundevents)):
+        costs = calculateMatchingCosts(events_left[i,0],foundevents[j][0])*100000
+        if costs < 0:
+            print("start")
+            print(events_left[i,:])
+            print(foundevents[j][:])
+            print("end")
+        disparity = int(abs(events_left[i][1]-foundevents[j][1]))
+        wmi[int(events_left[i][2]),int(events_left[i][1]),disparity] = costs
+    if i%500 == 0:
+        wmi = wmi *0.7
+        wmia = wmi.clip(min=0)
+        tmp = gaussian_filter(wmi, sigma=1)
+        disp = np.argmax(tmp, 2)
+        plt.imshow(disp, cmap = "hot")
+        plt.show()
 
 
 print("aaaaaaaaaaaaaaaaaaaaaaa")
