09f38bcf48fee6ac5709dfee501478eb4fe4dc8f
weiheng <weiheng.xia@gmail.com>
2020-07-01 10:52:33 +0200

    produce result_simple/ on the simplest data

--- main2.py
+++ main2.py
@@ -5,17 +5,17 @@ from CameraBuffer import CameraBuffer
 from OutputController import OutputController
 import os
 
-#filename_sub_left = 'data/berlinale/cam0/events.txt'
-#filename_sub_right = 'data/berlinale/cam1/events.txt'
-filename_sub_left = 'sim_flying_room_stereo/cam0/events.txt'
-filename_sub_right = 'sim_flying_room_stereo/cam1/events.txt'
+filename_sub_left = 'berlinale/cam0/events.txt'
+filename_sub_right = 'berlinale/cam1/events.txt'
+# filename_sub_left = 'sim_flying_room_stereo/cam0/events.txt'
+# filename_sub_right = 'sim_flying_room_stereo/cam1/events.txt'
 max_y = 180
 max_x = 240
 max_disparity = 50
 time_resolution = 0.0001
 maximum_timeslot = 1000
-if not os.path.exists("result/"):
-        os.makedirs("result/")
+if not os.path.exists("result_simple/"):
+        os.makedirs("result_simple/")
 
 a = CameraBuffer(max_x,max_y,filename_sub_left,filename_sub_right,max_disparity, time_resolution)
 a.prepareData()
