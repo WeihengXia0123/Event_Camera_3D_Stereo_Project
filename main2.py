7eebd2913ba7683fb35a4bc0625ec875b0c6e156
adina <adina.zell@campus.tu-berlin.de>
2020-07-01 10:04:45 +0200

    create result directory automatically, if it not exists

--- main2.py
+++ main2.py
@@ -3,6 +3,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from CameraBuffer import CameraBuffer
 from OutputController import OutputController
+import os
 
 #filename_sub_left = 'data/berlinale/cam0/events.txt'
 #filename_sub_right = 'data/berlinale/cam1/events.txt'
@@ -13,6 +14,9 @@ max_x = 240
 max_disparity = 50
 time_resolution = 0.0001
 maximum_timeslot = 1000
+if not os.path.exists("result/"):
+        os.makedirs("result/")
+
 a = CameraBuffer(max_x,max_y,filename_sub_left,filename_sub_right,max_disparity, time_resolution)
 a.prepareData()
 if False:
